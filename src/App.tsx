3f1ce9175bf7427b1e7449d7ce70e71f33417f04
Angelica Bayer <angelicacamboim@gmail.com>
2023-08-02 19:29:37 -0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,8 @@ export function App() {
 				<Header toggleTheme={toggleTheme} />
 				<Switch>
 					<Route path="/pokemon-react"  exact component={Home} />
-					<Route path="/pokemon-react/pokedex" exact component={Pokedex} />
-					<Route path="/pokemon-react/pokemon/:namePokemon" exact component={PokemonDetails} />
+					<Route path="/pokedex" exact component={Pokedex} />
+					<Route path="/pokemon/:namePokemon" exact component={PokemonDetails} />
 					<Route component={Page404} />
 				</Switch>
 				<Footer />
